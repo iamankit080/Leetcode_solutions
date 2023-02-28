be6f4907603cc69d6a0e07b9945bef69cb169366
Ankit Kumar <86267078+iamankitkr@users.noreply.github.com>
2023-02-28 10:03:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (83.50%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) 
+    {
+        //create a max heap and store all array elements
+     priority_queue<int> pq(stones.begin(),stones.end());
+        
+        //loop will run till size > 1
+        while(pq.size() > 1)
+        {
+            //take first element 
+            int f=pq.top();
+            
+            pq.pop();
+            //take 2nd element 
+            int s=pq.top();
+            pq.pop();
+            
+            //if both are not equal 
+            //and if both are equal numbers are destroyed from pq
+            if(f!=s)
+            {
+                pq.push(f-s);
+            }
+        }
+        
+        return pq.empty()?0:pq.top();
+    }
+};
