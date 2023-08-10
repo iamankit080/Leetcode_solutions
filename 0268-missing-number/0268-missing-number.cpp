b1742e7159bb17407c08a919c51e79928e216897
Ankit Kumar <86267078+iamankitkr@users.noreply.github.com>
2023-08-11 01:06:12 +0530

    Time: 19 ms (66.66%), Space: 17.9 MB (39.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,23 +1,13 @@
-
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        for (int i = 0; i <= nums.size(); i++) {
-            int flag = 0;
-            for (int j = 0; j < nums.size(); j++) {  // Corrected loop condition
-                if (nums[j] == i) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0) return i;
+        int n=nums.size();
+        int expsum=((n*(n+1))/2);
+        int actsum=0;
+        for(int i=0;i<n;i++){
+            actsum=actsum+nums[i];
         }
-        return -1;  // To handle the case where no missing number is found
+        return(expsum-actsum); 
+        
     }
 };
-
-
-
-
-
-
