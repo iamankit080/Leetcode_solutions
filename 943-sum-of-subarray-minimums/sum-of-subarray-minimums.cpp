6de12548aaddbb0a3a2c13023a012469ad9f768d
Ankit Kumar <ankitgupta8504@gmail.com>
2026-02-20 01:13:21 +0530

    Time: 13 ms (76.31%) | Memory: 47.1 MB (58.88%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,63 @@
+class Solution {
+
+    vector<int> findnse(vector<int> &arr){
+        int n=arr.size();
+        stack<int>st;
+        vector<int>ans(n);
+
+        for(int i=n-1; i>=0; i--){
+            int curr_ele = arr[i];
+
+            while(!st.empty() && arr[st.top()]>= arr[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            else ans[i]=n;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> findpsee(vector<int> &arr){
+        int n=arr.size();
+        stack<int>st;
+        vector<int>ans(n);
+
+        for(int i=0; i<n; i++){
+            int curr_ele = arr[i];
+
+            while(!st.empty() && arr[st.top()]> arr[i]){ //storing index not value
+                st.pop();
+            }
+
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            else ans[i]=-1;
+            st.push(i);
+        }
+        return ans;
+    }
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int mod =1e9+7;
+        int n = arr.size();
+        long long ans=0;
+        
+        vector<int>nse = findnse(arr);
+        vector<int>psee = findpsee(arr);
+
+        for(int i=0;i<n;i++){
+            int left = i-psee[i];
+            int right = nse[i] - i;
+
+            long long contribution = (( left * right * 1LL)% mod * arr[i]) % mod;
+            ans = (ans + contribution) % mod;
+
+        }
+        return (int)ans;
+    }
+};
